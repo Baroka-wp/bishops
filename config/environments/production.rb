b69b2dbea6a2948eb9fa61665bcdfcede9d60f32
BAROKA <birotori@gmail.com>
2020-10-28 21:52:28 +0100

    fixe devise default_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,5 +108,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'https://samurai-bishop.herokuapp.com/' }
 
+  config.action_mailer.delivery_method = :smtp
+  #host = 'localhost:3000'
+  host = 'https://samurai-bishop.herokuapp.com/'
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'birotori@gmail.com',
+    password: 'rikaelbiz',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
