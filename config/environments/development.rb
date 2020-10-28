cf1f9f8cd96dd649b01d5901ed4257f04b42d8c2
BAROKA <birotori@gmail.com>
2020-10-28 23:42:17 +0100

    google ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.default_url_options = { host: 'https://samurai-bishop.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  #config.action_mailer.default_url_options = { host: 'https://samurai-bishop.herokuapp.com/' }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -64,17 +64,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.delivery_method = :smtp
-  #host = 'localhost:3000'
-  host = 'https://samurai-bishop.herokuapp.com/'
-  
+  host = 'localhost:3000'
+  #host = 'https://samurai-bishop.herokuapp.com/'
+
 
   # SMTP settings for gmail
-  #config.action_mailer.smtp_settings = {
-  #  address: 'smtp.gmail.com',
-  #  port: 587,
-  #  user_name: ENV['GOOGLE_USERNAME'],
-  #  password: ENV['GOOGLE_PASSWORD'],
-  #  authentication: 'plain',
-  #  enable_starttls_auto: true
-  #}
+  config.action_mailer.smtp_settings = {
+   address: 'smtp.gmail.com',
+   port: 587,
+   user_name: ENV['GOOGLE_USERNAME'],
+   password: ENV['GOOGLE_PASSWORD'],
+   authentication: 'plain',
+   enable_starttls_auto: true
+  }
 end
