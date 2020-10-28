14664308701ac9637d475ec9b2fdfed66313df6c
BAROKA <birotori@gmail.com>
2020-10-28 22:14:15 +0100

    google ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'birotori@gmail.com',
-    password: 'rikaelbiz',
+    user_name: ENV['GOOGLE_USERNAME'],
+    password: ENV['GOOGLE_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
