88802227b04a23222ce1cef4f2715eb210ad81f0
BAROKA <birotori@gmail.com>
2020-10-28 21:36:58 +0100

    host heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'https://samurai-bishop.herokuapp.com/' }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -63,8 +64,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
-
+  #host = 'localhost:3000'
+  host = 'https://samurai-bishop.herokuapp.com/'
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
