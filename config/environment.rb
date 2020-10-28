804b223ff30bae6fd052c75160123ff84ff9f562
BAROKA <birotori@gmail.com>
2020-10-28 22:32:36 +0100

    send grid env

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'ENV['SENDGRID_USERNAME']',
-  :password => 'ENV['SENDGRID_PASSWORD']',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'yourdomain.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
