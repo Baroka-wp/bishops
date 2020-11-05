06a06a15788101b14dc0e699f6011aef0ddcd273
baroka <birotori@gmail.com>
2020-11-05 20:22:15 +0100

    STPM for heroku change name

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module Bishop
     host = 'localhost:3000' # replace with your own url
     config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
 
-    #host = 'https://obscure-brushlands-27266.herokuapp.com/' # replace with your own url
-    #config.action_mailer.default_url_options = { host: 'https://obscure-brushlands-27266.herokuapp.com/', protocol: 'http' }
+    host = 'https://bishop-samurai.herokuapp.com/' # replace with your own url
+    config.action_mailer.default_url_options = { host: 'https://bishop-samurai.herokuapp.com/', protocol: 'http' }
     # SMTP settings for gmail
     config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
